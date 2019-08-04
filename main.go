307ef5323968c3905c7e03bd8044a44eace9c7dd
qiankaihua <qiankaihua@126.com>
2019-08-04 23:35:48 +0800

    upd init and usage

--- main.go
+++ main.go
@@ -11,15 +11,31 @@ import (
 func _init() {
 	Config.InitConfig()
 	Log.InitLog()
+	Log.InitTimer()
 	Orm.InitOrm()
+
 	// use Orm.GetDB to get orm instance
 	//db := Orm.GetDB()
 	//db.HasTable("test")
-	Log.PushNotice("a", "123")
-	Log.PushNotice("b", "abc")
-	Log.PrintNotice()
+
+	// append notice, [key=value]
+	//Log.PushNotice("a", "123")
+	//Log.PushNotice("b", "abc")
+
+	// SetTimer 设置定时器
+	//Log.SetTimer("test")
+
 	Http.InitHttp()
 	Route.AddApiRoute()
+
+	// GetTimer 获取定时器时间
+	//Log.PushNotice("test_timer", Log.GetTimer("test"))
+
+	// 打印 PushNotice 的日志
+	//Log.PrintNotice()
+
+	// 设置 Error 级别的 log
+	//Log.Error("Error Log")
 }
 func _end() {
 	Orm.EndOrm()
